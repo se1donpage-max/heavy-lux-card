549e7c50bc87b0d792226f43d2f87bffe04e7df2
se1donpage-max <se1don.page@gmail.com>
2026-08-19 18:45:54 +0300

    Create game.js

---
+++ config/game.js
@@ -0,0 +1,29 @@
+module.exports = {
+  GAME_NAME: "Heavy Lux Card",
+
+  DECK_SIZE: 36,
+
+  HAND_SIZE: 6,
+
+  MAX_PLAYERS_PER_ROOM: 2,
+
+  ROOM_CODE_LENGTH: 6,
+
+  MAX_CHAT_MESSAGES: 100,
+
+  RECONNECT_GRACE_MS: 120000,
+
+  STARTING_LEVEL: 1,
+
+  MAX_LEVEL: 100,
+
+  XP_PER_LEVEL: 100,
+
+  TITLES: {
+    20: "Член клуба",
+    40: "Старший член клуба",
+    60: "Почётный член клуба",
+    80: "Попечитель",
+    100: "Покровитель"
+  }
+};
