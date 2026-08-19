9fc77647bf71edd0dfbaf5ee075ee75e43d1be67
se1donpage-max <se1don.page@gmail.com>
2026-08-19 18:47:40 +0300

    Create cars.js

---
+++ config/cars.js
@@ -0,0 +1,176 @@
+module.exports = {
+  HEAVY_MOTORS: [
+    {
+      id: "bmw_3_g20",
+      brand: "BMW",
+      model: "3 Series G20",
+      price: 7000000,
+      stock: 50
+    },
+    {
+      id: "bmw_5_g60",
+      brand: "BMW",
+      model: "5 Series G60",
+      price: 9500000,
+      stock: 40
+    },
+    {
+      id: "bmw_7_g70",
+      brand: "BMW",
+      model: "7 Series G70",
+      price: 15000000,
+      stock: 30
+    },
+    {
+      id: "bmw_x5",
+      brand: "BMW",
+      model: "X5",
+      price: 12000000,
+      stock: 40
+    },
+    {
+      id: "mercedes_e",
+      brand: "Mercedes-Benz",
+      model: "E-Class",
+      price: 9000000,
+      stock: 40
+    },
+    {
+      id: "mercedes_s",
+      brand: "Mercedes-Benz",
+      model: "S-Class",
+      price: 18000000,
+      stock: 25
+    },
+    {
+      id: "mercedes_gle",
+      brand: "Mercedes-Benz",
+      model: "GLE",
+      price: 12500000,
+      stock: 35
+    },
+    {
+      id: "audi_a6",
+      brand: "Audi",
+      model: "A6",
+      price: 8500000,
+      stock: 40
+    },
+    {
+      id: "audi_a8",
+      brand: "Audi",
+      model: "A8",
+      price: 14000000,
+      stock: 25
+    },
+    {
+      id: "audi_q8",
+      brand: "Audi",
+      model: "Q8",
+      price: 13000000,
+      stock: 30
+    },
+    {
+      id: "porsche_911",
+      brand: "Porsche",
+      model: "911 Carrera",
+      price: 22000000,
+      stock: 15
+    },
+    {
+      id: "porsche_cayenne",
+      brand: "Porsche",
+      model: "Cayenne",
+      price: 18000000,
+      stock: 20
+    },
+    {
+      id: "mustang_gt",
+      brand: "Ford",
+      model: "Mustang GT",
+      price: 8000000,
+      stock: 35
+    },
+    {
+      id: "range_rover_sport",
+      brand: "Land Rover",
+      model: "Range Rover Sport",
+      price: 15000000,
+      stock: 25
+    },
+    {
+      id: "range_rover_autobiography",
+      brand: "Land Rover",
+      model: "Range Rover Autobiography",
+      price: 22000000,
+      stock: 15
+    }
+  ],
+
+  HEAVY_EXCLUSIVE: [
+    {
+      id: "rolls_royce_mansory",
+      brand: "Rolls-Royce",
+      model: "Mansory",
+      tuner: "Mansory",
+      price: 120000000,
+      stock: 5
+    },
+    {
+      id: "bmw_alpina_b7",
+      brand: "BMW",
+      model: "Alpina B7",
+      tuner: "Alpina",
+      price: 55000000,
+      stock: 8
+    },
+    {
+      id: "mercedes_g63_brabus",
+      brand: "Mercedes-Benz",
+      model: "G63 Brabus",
+      tuner: "Brabus",
+      price: 75000000,
+      stock: 8
+    },
+    {
+      id: "porsche_911_rocket",
+      brand: "Porsche",
+      model: "911 Rocket Edition",
+      tuner: "TechArt",
+      price: 65000000,
+      stock: 6
+    },
+    {
+      id: "bmw_m8_hamann",
+      brand: "BMW",
+      model: "M8 Hamann",
+      tuner: "Hamann",
+      price: 50000000,
+      stock: 8
+    },
+    {
+      id: "mercedes_s_brabus",
+      brand: "Mercedes-Benz",
+      model: "S-Class Brabus",
+      tuner: "Brabus",
+      price: 85000000,
+      stock: 5
+    },
+    {
+      id: "range_rover_mansory",
+      brand: "Range Rover",
+      model: "Mansory",
+      tuner: "Mansory",
+      price: 70000000,
+      stock: 7
+    },
+    {
+      id: "porsche_turbo_s_techart",
+      brand: "Porsche",
+      model: "911 Turbo S",
+      tuner: "TechArt",
+      price: 60000000,
+      stock: 7
+    }
+  ]
+};
