fc302efebbcbef2250fbba7c9b593f96ac20e4da
se1donpage-max <se1don.page@gmail.com>
2026-08-19 18:48:51 +0300

    Create businesses.js

---
+++ config/businesses.js
@@ -0,0 +1,42 @@
+module.exports = [
+  {
+    id: "nightclub",
+    name: "Ночной клуб",
+    price: 25000000,
+    income: 350000,
+    expenses: 120000,
+    maxPerPlayer: 3
+  },
+  {
+    id: "restaurant",
+    name: "Ресторан",
+    price: 35000000,
+    income: 480000,
+    expenses: 160000,
+    maxPerPlayer: 3
+  },
+  {
+    id: "hotel",
+    name: "Гостиница",
+    price: 60000000,
+    income: 850000,
+    expenses: 300000,
+    maxPerPlayer: 3
+  },
+  {
+    id: "factory",
+    name: "Завод",
+    price: 100000000,
+    income: 1400000,
+    expenses: 550000,
+    maxPerPlayer: 3
+  },
+  {
+    id: "oil_terminal",
+    name: "Нефтяной терминал",
+    price: 250000000,
+    income: 3500000,
+    expenses: 1400000,
+    maxPerPlayer: 3
+  }
+];
