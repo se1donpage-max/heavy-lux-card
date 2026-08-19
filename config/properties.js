5dee8a139fc0454b950217a48e1c4f70888d1d9b
se1donpage-max <se1don.page@gmail.com>
2026-08-19 18:49:20 +0300

    Create properties.js

---
+++ config/properties.js
@@ -0,0 +1,32 @@
+module.exports = [
+  {
+    id: "cottage",
+    name: "Коттеджный дом",
+    price: 30000000,
+    income: 50000
+  },
+  {
+    id: "estate",
+    name: "Усадьба",
+    price: 60000000,
+    income: 100000
+  },
+  {
+    id: "seaside_penthouse",
+    name: "Пентхаус у моря",
+    price: 120000000,
+    income: 200000
+  },
+  {
+    id: "palace",
+    name: "Дворец",
+    price: 250000000,
+    income: 450000
+  },
+  {
+    id: "imperial_palace",
+    name: "Императорский дворец",
+    price: 500000000,
+    income: 900000
+  }
+];
